7eb252408b171cd620148d42e7d342c3f84ad5bb
Mathluiz23 <matheus_luizs@hotmail.com>
2024-09-11 09:57:38 -0300

    :sparkles:feat(migration): create Transactions table with necessary fields

---
+++ backend/migrations/20240910005446-create-transaction.js
@@ -0,0 +1,59 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      paymentMethod: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      expenseType: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      transactionType: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      accumulatedBalance: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      observations: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  }
+};

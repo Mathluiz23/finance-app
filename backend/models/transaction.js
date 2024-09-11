d87b7cfde55c90e47a2ddfb622ee463cd8348b78
Mathluiz23 <matheus_luizs@hotmail.com>
2024-09-11 10:02:47 -0300

    :sparkles:feat(models): define Transaction model with necessary fields

---
+++ backend/models/transaction.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Transaction.init({
+    date: DataTypes.DATE,
+    description: DataTypes.STRING,
+    category: DataTypes.STRING,
+    paymentMethod: DataTypes.STRING,
+    value: DataTypes.DECIMAL,
+    expenseType: DataTypes.STRING,
+    transactionType: DataTypes.STRING,
+    accumulatedBalance: DataTypes.DECIMAL,
+    observations: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+  });
+  return Transaction;
+};

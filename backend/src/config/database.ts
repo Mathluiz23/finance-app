df1d01e882b746ea38f81ad20e1ec5ef795b0070
Mathluiz23 <matheus_luizs@hotmail.com>
2024-09-16 22:13:34 -0300

    :truck: (chore): moving files and folders

--- backend/src/config/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize(process.env.DB_NAME || 'finance_db', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '', {
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'mysql',
-});
-
-export default sequelize;
